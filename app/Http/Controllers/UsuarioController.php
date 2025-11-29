5bbb046c5cad6977f62fea2421d5bd29e3add690
JoosX <daniel19092005ortecho@gmail.com>
2025-11-29 14:29:26 -0500

    removi lo que estaba mal y el intento de apis

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException; 
 
 class UsuarioController extends Controller
 {
-
+    
 }
- 
